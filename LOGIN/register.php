ad090f01203dc8d9f5a09fc5f5c17cc41ee18fc9
Araidee <126244296+Araidee5555@users.noreply.github.com>
2025-02-19 08:04:41 +0000

    Update register.php

--- LOGIN/register.php
+++ LOGIN/register.php
@@ -4,7 +4,6 @@ include 'connect.php';
 
 if(isset($_POST['signUp'])){
     $firstName=$_POST['fName'];
-    $lastName=$_POST['lName'];
     $email=$_POST['email'];
     $password=$_POST['password'];
     $password=md5($password);
