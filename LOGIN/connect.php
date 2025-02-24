c0df639dd9f7828280d6d4de8619313550c793d9
Araidee <126244296+Araidee5555@users.noreply.github.com>
2025-02-24 17:01:40 +0700

    Update connect.php

--- LOGIN/connect.php
+++ LOGIN/connect.php
@@ -2,9 +2,8 @@
 
 $host="localhost";
 $user="root";
-$pass="";
 $db="NDAS";
-$conn=new mysqli($host,$user,$pass,$db);
+$conn=new mysqli($host,$user,$db);
 if($conn->connect_error){
     echo "Failed to connect DB".$conn->connect_error;
 }
